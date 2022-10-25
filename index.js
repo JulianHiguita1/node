a4c78f8e7de5cc93e1303fce64eea81c2d291166
unknown <SENA@ITAAPRREAFSD352.FormacionSena.RED>
2022-10-25 16:14:59 -0500

    Commit ultimo hecho en clase

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const app = express();
+
+//Muestra en el localhost
+app.get('/', (req, res) => {
+    res.send('Hablamelo care lagartija')
+})
+
+//Escucha el puerto e imprime
+app.listen(3000, ()=> {
+    console.log('estoy en el puerto 3000')
+})
